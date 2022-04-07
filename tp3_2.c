67370a909f457b43755dc26bbaf3719623c9f01d
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-04-07 19:10:00 -0300

    primer punto hecho

---
+++ tp3_2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define cantidad 5
+
+void main () {
+    char *V[5];
+    char * buff;
+    printf ("\nIngrese los nombres:\n ");
+    for (int i=0;i<cantidad;i++) {
+        buff= (char *) malloc(100*sizeof(char));
+        gets(buff);
+        V[i]= (char *) malloc ((strlen(buff)+1)*sizeof(char));
+        strcpy(V[i],buff);
+    }
+    printf("\nLos nombres escritos son:\n ");
+    for (int i=0;i<cantidad;i++) {
+        puts(V[i]);
+    }
+}
